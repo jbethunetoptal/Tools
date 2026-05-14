337baba21df9bfee4ce32012803edfc918c6788c
Jonathan Bethune <45943895+jbethunetoptal@users.noreply.github.com>
2026-05-14 16:20:23 +0900

    Update rename.py
    Ninchi test

--- rename.py
+++ rename.py
@@ -6,8 +6,8 @@ folder = ""
 
 for file in os.listdir(folder):
     if file.endswith(".jfif"):
-        print("Changing this one: " + file)
+        print("Changing this one now: " + file)
         old_name = os.path.join(folder, file)
         new_name = old_name[:-5]+".jpg"
-        print("New name is this: " + new_name)
+        print("New name is this now: " + new_name)
         os.rename(old_name, new_name)
